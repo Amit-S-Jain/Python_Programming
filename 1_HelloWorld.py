81f3fc91279006d47ed5c8492fa1b5209dcda132
Amit Jain <Amitjain@github.com>
2020-08-01 11:29:44 +0530

    Create 1_HelloWorld.py

--- 1_HelloWorld.py
+++ 1_HelloWorld.py
@@ -1,8 +1,9 @@
 print("Hello World!!!")	
 
+#Here '#' is used for comments in Python
 #In python it is very easy to display anything on Desktop. we just need to call the function print()
 #The print function need some parameters.
-#we can read any function details with using one more function named as help() function.
+#We can read any function details with using one more function named as help() function.
 #Help function is used to read all the information regarding that function.
 #Before studying print function wee will study the help function.
 #Help function need a parameter which will be python keyword or python function.
@@ -10,7 +11,9 @@ print("Hello World!!!")
 #help(function_name)
 #ex. help(print)
 
-help(print)
+#help(print)
+
+#Remove the '#' from above line and run again
 
 #Here print function needs 3 parameters as shown in help.
 #we got all the information regarding print function in help section.
