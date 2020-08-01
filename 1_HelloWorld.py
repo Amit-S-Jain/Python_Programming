99c12df759e59e6fac07f1e553a8f61fe42312a8
Amit Jain <Amitjain@github.com>
2020-08-01 11:28:10 +0530

    Create 1_HelloWorld.py

---
+++ 1_HelloWorld.py
@@ -0,0 +1,16 @@
+print("Hello World!!!")	
+
+#In python it is very easy to display anything on Desktop. we just need to call the function print()
+#The print function need some parameters.
+#we can read any function details with using one more function named as help() function.
+#Help function is used to read all the information regarding that function.
+#Before studying print function wee will study the help function.
+#Help function need a parameter which will be python keyword or python function.
+#Syntax of help function is as follows:
+#help(function_name)
+#ex. help(print)
+
+help(print)
+
+#Here print function needs 3 parameters as shown in help.
+#we got all the information regarding print function in help section.
